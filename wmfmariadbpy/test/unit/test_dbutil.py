ac32d9f980238a4807f42e8825bcf1cb3350cfca
Stevie Shirley <sshirley@wikimedia.org>
2021-02-05 15:13:49 +0100

    tox/unit: Allow unit tests to be indepdenent of env vars
    The current tox config will set $DBUTIL_SECTION_PORTS unless it's
    running the 'unit' target. This breaks when you try to use 'cover'
    target, and there doesn't seem to be a way to exclude both.
    
    So, instead this change makes the unit tests reference the same
    section_ports.csv file, and be independent of whether
    $DBUTIL_SECTION_PORTS is set in the current environment or not.
    
    Change-Id: I7799a8e22b191285a9bf4d92a81ef4fb9f6ffc73

--- wmfmariadbpy/test/unit/test_dbutil.py
+++ wmfmariadbpy/test/unit/test_dbutil.py
@@ -1,22 +1,24 @@
+import os.path
+
 import pytest
 
 import wmfmariadbpy.dbutil as dbutil
 
-csv_data = ("s1, 1", "s2, 2", "m3, 100")
+
+@pytest.fixture(autouse=True)
+def set_dbutil_section_ports(monkeypatch):
+    # This is a bit ugly, but needed to make it work without being dependent
+    # on the current dir.
+    monkeypatch.setenv(
+        dbutil.DBUTIL_SECTION_PORTS_ENV,
+        os.path.join(os.path.dirname(__file__), "..", "section_ports.csv"),
+    )
 
 
-@pytest.fixture
-def mock_csv(mocker):
-    m = mocker.patch("builtins.open", mocker.mock_open())
-    # XXX(kormat): Work-around python < 3.7's mock_open not supporting iteration.
-    # https://bugs.python.org/issue21258#msg285508
-    m.return_value.__iter__ = lambda self: iter(csv_data)
-
-
-def test_read_section_ports_list(mock_csv):
+def test_read_section_ports_list():
     port2sec, sec2port = dbutil.read_section_ports_list()
-    assert port2sec[2] == "s2"
-    assert sec2port["m3"] == 100
+    assert port2sec[10112] == "f2"
+    assert sec2port["alpha"] == 10320
 
 
 @pytest.mark.parametrize(
@@ -29,6 +31,8 @@ def test_read_section_ports_list(mock_csv):
     ],
 )
 def test_read_section_ports_list_env_path(monkeypatch, mocker, path, setenv, expected):
+    # Unset the env var so this test is hermetic.
+    monkeypatch.delenv(dbutil.DBUTIL_SECTION_PORTS_ENV, raising=False)
     mocker.patch("wmfmariadbpy.dbutil.SECTION_PORT_LIST_FILE", "/default")
     m = mocker.patch("builtins.open", mocker.mock_open())
     if setenv:
@@ -40,52 +44,52 @@ def test_read_section_ports_list_env_path(monkeypatch, mocker, path, setenv, exp
 @pytest.mark.parametrize(
     "section,port",
     [
-        ("m3", 100),
+        ("alpha", 10320),
         ("nonexistent", 3306),
         (None, 3306),
     ],
 )
-def test_get_port_from_section(mock_csv, section, port):
+def test_get_port_from_section(section, port):
     assert dbutil.get_port_from_section(section) == port
 
 
 @pytest.mark.parametrize(
     "port, section",
     [
-        (2, "s2"),
-        (100, "m3"),
+        (10112, "f2"),
+        (10320, "alpha"),
         (655359, None),
         (-1, None),
         (0, None),
         (None, None),
     ],
 )
-def test_get_section_from_port(mock_csv, port, section):
+def test_get_section_from_port(port, section):
     assert dbutil.get_section_from_port(port) == section
 
 
 @pytest.mark.parametrize(
     "port, datadir",
     [
-        (1, "/srv/sqldata.s1"),
-        (100, "/srv/sqldata.m3"),
+        (10110, "/srv/sqldata.f0"),
+        (10320, "/srv/sqldata.alpha"),
         (101, "/srv/sqldata"),
         (None, "/srv/sqldata"),
     ],
 )
-def test_get_datadir_from_port(mock_csv, port, datadir):
+def test_get_datadir_from_port(port, datadir):
     assert dbutil.get_datadir_from_port(port) == datadir
 
 
 @pytest.mark.parametrize(
     "port, sock",
     [
-        (2, "/run/mysqld/mysqld.s2.sock"),
+        (10111, "/run/mysqld/mysqld.f1.sock"),
         (101, "/run/mysqld/mysqld.sock"),
         (None, "/run/mysqld/mysqld.sock"),
     ],
 )
-def test_get_socket_from_port(mock_csv, port, sock):
+def test_get_socket_from_port(port, sock):
     assert dbutil.get_socket_from_port(port) == sock
 
 
@@ -94,13 +98,13 @@ def test_get_socket_from_port(mock_csv, port, sock):
     [
         ("localhost", "localhost", 3306),
         ("localhost:3311", "localhost", 3311),
-        ("localhost:s1", "localhost", 1),
+        ("localhost:f1", "localhost", 10111),
         ("db1001", "db1001.eqiad.wmnet", 3306),
         ("db5999:3321", "db5999.eqsin.wmnet", 3321),
-        ("db4999:m3", "db4999.ulsfo.wmnet", 100),
+        ("db4999:alpha", "db4999.ulsfo.wmnet", 10320),
         ("db2001.codfw.wmnet", "db2001.codfw.wmnet", 3306),
         ("dbmonitor1001.wikimedia.org", "dbmonitor1001.wikimedia.org", 3306),
     ],
 )
-def test_resolve(mock_csv, target, host, port):
+def test_resolve(target, host, port):
     assert dbutil.resolve(target) == (host, port)
