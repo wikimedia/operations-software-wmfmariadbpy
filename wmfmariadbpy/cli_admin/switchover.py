41d2acdfff46edbe42551ad4e3a039e489ee5f7d
Marostegui <marostegui@wikimedia.org>
2024-07-08 08:00:09 +0000

    switchover.py: Add sudo curl
    * switchover.py: Add sudo curl
    
    sudo is needed to be able to run this command as non root

--- wmfmariadbpy/cli_admin/switchover.py
+++ wmfmariadbpy/cli_admin/switchover.py
@@ -649,12 +649,12 @@ def update_events(master, slave):
         )
     )
     print(
-        "curl -sS 'https://gerrit.wikimedia.org/r/plugins/gitiles/operations/software/+/refs/heads/master/dbtools/events_coredb_slave.sql?format=TEXT' | base64 -d | db-mysql {}".format(
+        "curl -sS 'https://gerrit.wikimedia.org/r/plugins/gitiles/operations/software/+/refs/heads/master/dbtools/events_coredb_slave.sql?format=TEXT' | base64 -d | sudo db-mysql {}".format(
             master
         )
     )
     print(
-        "curl -sS 'https://gerrit.wikimedia.org/r/plugins/gitiles/operations/software/+/refs/heads/master/dbtools/events_coredb_master.sql?format=TEXT' | base64 -d | db-mysql {}".format(
+        "curl -sS 'https://gerrit.wikimedia.org/r/plugins/gitiles/operations/software/+/refs/heads/master/dbtools/events_coredb_master.sql?format=TEXT' | base64 -d | sudo db-mysql {}".format(
             slave
         )
     )
